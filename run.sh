67fde1d9d11a1de37077f94f2b7c831d729821b4
Artyom K. <artyomkarpov@gmail.com>
2023-11-15 12:40:27 +0300

    init

---
+++ run.sh
@@ -0,0 +1,9 @@
+# Provision demo site:
+# - Download https://github.com/docker/awesome-compose/tree/master/react-express-mysql
+
+# Scan local ports using nmap and esp. 3000 port to check if a web server sitting on a port:
+nmap -p 3000 localhost
+
+
+
+
